d03265ee3cc9a0fc2ab81b7b47d3cd09b4d75aa9
alokvns48 <alokvns48@gmail.com>
2024-05-10 16:51:06 +0530

    react frontend

--- qrmenu_backend/core/views.py
+++ qrmenu_backend/core/views.py
@@ -1,3 +1,39 @@
+import json
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 from django.shortcuts import render
 
+from rest_framework import generics
+from . import models, serializers, permissions
+
 # Create your views here.
+class PlaceList(generics.ListCreateAPIView):
+  serializer_class = serializers.PlaceSerializer
+
+  def get_queryset(self):
+    return models.Place.objects.filter(owner_id=self.request.user.id)
+
+  def perform_create(self, serializer):
+    serializer.save(owner=self.request.user)
+    
+class PlaceDetail(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = [permissions.IsOwnerOrReadOnly]
+  serializer_class = serializers.PlaceDetailSerializer
+  queryset = models.Place.objects.all()
+
+class CategoryList(generics.CreateAPIView):
+  permission_classes = [permissions.PlaceOwnerOrReadOnly]
+  serializer_class = serializers.CategorySerializer
+
+class CategoryDetail(generics.UpdateAPIView, generics.DestroyAPIView):
+  permission_classes = [permissions.PlaceOwnerOrReadOnly]
+  serializer_class = serializers.CategorySerializer
+  queryset = models.Category.objects.all()
+
+
+def home(request):
+  return render(request, 'index.html')
+
+
---
+++ qrmenu_frontend/src/index.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./router/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
